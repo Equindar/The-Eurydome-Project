85db8eb8d77af63f50c751da1c23d5096ef09939
Equindar <equindar@gmail.com>
2024-02-29 14:50:02 +0100

    some progress, testing

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,16 @@ import Logger from 'core/Logger';
 import express from 'express';
 import status from './status/status';
 
+/**
+ * Router Instance of express
+ */
 const router = express.Router();
 
+/**
+ * @summary this is a summary
+ * @param no params required
+ * @returns the status of the server
+ */
 router.use('/status', status);
 
 Logger.info('Express Router: /');
