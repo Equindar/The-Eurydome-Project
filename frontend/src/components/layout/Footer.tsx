bfd0df428533e1c7aa36238a670e705b2fab792c
Equindar <equindar@gmail.com>
2024-03-13 17:25:52 +0100

    feat(svg): added svg support

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from '@/lib/i18n';
+import { ReactComponent as Envelope } from 'media/assets/EnvelopeIcon.svg';
 import LanguageSelect from '../language/LanguageSelect';
 
 interface FooterProps {
@@ -12,70 +13,11 @@ const Footer = async (props: FooterProps) => {
       <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com" className="flex items-center">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="mr-3 h-8"
-                alt="Logo"
-              />
-              <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-                {t('title')}
-              </span>
-            </a>
+            <Envelope width={25} className='dark:text-white'/>
+            <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+              {t('title')}
+            </span>
             <LanguageSelect />
-          </div>
-          <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
-                Resources
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400">
-                <li className="mb-4">
-                  <a href="https://flowbite.com" className="hover:underline">
-                    Blog
-                  </a>
-                </li>
-                <li>
-                  <a href="https://tailwindcss.com/" className="hover:underline">
-                    Patch Notes
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
-                Follow us
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400">
-                <li className="mb-4">
-                  <a href="https://github.com/themesberg/flowbite" className="hover:underline ">
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">
-                    Discord
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
-                Legal
-              </h2>
-              <ul className="text-gray-600 dark:text-gray-400">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Terms &amp; Conditions
-                  </a>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
         <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
