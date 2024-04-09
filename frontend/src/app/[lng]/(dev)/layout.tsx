248a567f98cc9d060d9f6439da140d24fccf3f19
Equindar <equindar@gmail.com>
2024-04-09 16:43:50 +0200

    test(assets): minor testing to dev background (dotted svg) for dashboard

--- frontend/src/app/[lng]/(dev)/layout.tsx
+++ frontend/src/app/[lng]/(dev)/layout.tsx
@@ -21,7 +21,10 @@ export default function Layout({ children, lng }: RootLayoutProps) {
         className="relative min-h-screen w-full bg-gradient-to-r from-cyan-200 to-blue-400
         dark:from-cyan-700 dark:to-blue-900"
       >
-        <div id="content-wrap" className="px-4 py-24">
+        <div
+          id="content-wrap"
+          className="min-h-full bg-[url('/assets/dotted-background.svg')] px-4 py-24"
+        >
           {children}
         </div>
         <Footer lng={lng} />
